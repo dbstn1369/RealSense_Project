c846fdd1e8194f3ac490cb78ac5351784dbe9a0c
Choiyoonsoo <dbstn1369@gmail.com>
2018-11-22 19:26:35 +0900

    Create crop.py
    원하는 부분의 픽셀을 설정하면 네모크기의 부분을 numpy로 저장

---
+++ upgrade_crop/crop.py
@@ -0,0 +1,96 @@
+import pyrealsense2 as rs
+import time
+import numpy as np
+import cv2
+
+
+
+
+pipeline = rs.pipeline()
+
+#Create a config and configure the pipeline to stream
+#  different resolutions of color and depth streams
+config = rs.config()
+config.enable_stream(rs.stream.depth, 640, 360, rs.format.z16, 30)
+config.enable_stream(rs.stream.color, 640, 480, rs.format.bgr8, 30)
+
+# Start streaming
+profile = pipeline.start(config)
+
+# Getting the depth sensor's depth scale (see rs-align example for explanation)
+depth_sensor = profile.get_device().first_depth_sensor()
+depth_scale = depth_sensor.get_depth_scale()
+print("Depth Scale is: " , depth_scale)
+
+# We will be removing the background of objects more than
+#  clipping_distance_in_meters meters away
+clipping_distance_in_meters = 1 #1 meter
+clipping_distance = clipping_distance_in_meters / depth_scale
+
+# Create an align object
+# rs.align allows us to perform alignment of depth frames to others frames
+# The "align_to" is the stream type to which we plan to align depth frames.
+align_to = rs.stream.color
+align = rs.align(align_to)
+
+def crop_center_color(img,cropx,cropy):
+    y,x,c = img.shape
+    print(x)
+    print(y)
+    startx = x//2 - cropx//2
+    starty = y//2 - cropy//2
+    return img[starty:starty+cropy, startx:startx+cropx, :]
+
+def crop_center_depth(img,cropx,cropy):
+    y,x = img.shape
+    startx = x//2 - cropx//2
+    starty = y//2 - cropy//2
+    return img[starty:starty+cropy, startx:startx+cropx]
+
+index = 0
+try:
+
+    while index < 1:  #added this
+        time.sleep(1)
+        # Wait for the next set of frames from the camera
+        frames = pipeline.wait_for_frames()
+
+        aligned_frames = align.process(frames)
+
+        aligned_depth_frame = aligned_frames.get_depth_frame()  # aligned_depth_frame is a 640x480 depth image
+        color_frame = aligned_frames.get_color_frame()
+
+        if not aligned_depth_frame or not color_frame:
+            continue
+
+        depth_image = np.asanyarray(aligned_depth_frame.get_data())
+        color_image = np.asanyarray(color_frame.get_data())
+
+        print(depth_image.shape)
+
+        depth_image = crop_center_depth(depth_image, 300, 400); #이부분을 viewr로 설정해서 조절하면 됩니다.
+
+        color_image = crop_center_color(color_image, 300, 400);
+
+        print(color_image.shape)
+
+
+
+        grey_color = 153
+        depth_image_3d = np.dstack(
+            (depth_image, depth_image, depth_image))  # depth image is 1 channel, color is 3 channels
+        bg_removed = np.where((depth_image_3d > clipping_distance) | (depth_image_3d <= 0), grey_color, color_image)
+
+        depth_colormap = cv2.applyColorMap(cv2.convertScaleAbs(depth_image, alpha=0.03), cv2.COLORMAP_JET)
+        images = np.hstack((bg_removed, depth_colormap))
+        print(type(images))
+        cv2.namedWindow('Align Example', cv2.WINDOW_AUTOSIZE)
+        cv2.imshow('Align Example', images)
+        key = cv2.waitKey(1)
+        # Press esc or 'q' to close the image window
+        if key & 0xFF == ord('q') or key == 27:
+            cv2.destroyAllWindows()
+            break
+
+finally:
+    pipeline.stop()
